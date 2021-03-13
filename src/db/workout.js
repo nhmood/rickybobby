6c83b67f96ee65543864a1943453ad587677244e
Nima H. Mood <nhmood@goosecode.com>
2021-03-13 14:03:47 -0500

    Filter common workouts to cycling only and add (temp) recent workout helper

--- src/db/workout.js
+++ src/db/workout.js
@@ -9,20 +9,44 @@ class Workout extends Model {
       SELECT DISTINCT
         workoutA.*
       FROM
-        workouts as workoutA
+        workouts AS workoutA
       INNER JOIN
-        workouts as workoutB
+        workouts AS workoutB
       ON
         workoutA.ride_id  = workoutB.ride_id AND
         workoutA.user_id != workoutB.user_id
       WHERE
-        workoutA.user_id IN (?, ?);
+        workoutA.user_id IN (?, ?) AND
+        workoutA.fitness_discipline = 'cycling';
     `;
+
+
+
     const stmt = this.db.prepare(sql);
     const records = stmt.all(userA.id, userB.id);
 
     const models = records.map(r => { return new this(r) });
 
+    console.log({models});
+    return models;
+  }
+
+
+  // Get recent workouts
+  // TODO - put proper order by syntax into Model.where
+  static recent(limit){
+    let sql = `SELECT * FROM workouts ORDER BY created_at asc`;
+
+    // If the limit parameter is present and a valid integer, append to the SQL statement
+    let limitInt = parseInt(limit);
+    if (limitInt){
+      sql = sql.concat(` LIMIT ${limitInt}`);
+    }
+
+    const stmt = this.db.prepare(sql);
+    const records = stmt.all();
+
+    const models = records.map(r => { return new this(r) });
     console.log({models});
     return models;
   }

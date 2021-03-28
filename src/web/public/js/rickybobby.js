1ca45d7375572464761075b4c8ede7c76beaa115
Nima H. Mood <nhmood@goosecode.com>
2021-03-28 16:05:57 -0400

    Add updated home page with shake and bake search + recent activity

--- src/web/public/js/rickybobby.js
+++ src/web/public/js/rickybobby.js
@@ -1,1 +1,21 @@
 console.log("rickybobby v1.0");
+
+
+document.addEventListener("DOMContentLoaded", function(){
+  document.body.classList.remove("preload");
+});
+
+
+let searchButton = document.getElementById("searchButton");
+if (searchButton){ searchButton.addEventListener("click", searchUsers); }
+
+function searchUsers(){
+  let userA = document.getElementById("userA");
+  let userB = document.getElementById("userB");
+
+  let usernameA = userA.value;
+  let usernameB = userB.value;
+
+  let searchURL = `/shakeandbake?users=${usernameA},${usernameB}`;
+  window.location.href = searchURL;
+}

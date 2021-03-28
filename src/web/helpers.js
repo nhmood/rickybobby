9b823e47b974cf63c9ff4cc8a4d5f43d7c818ad9
Nima H. Mood <nhmood@goosecode.com>
2021-03-28 12:21:46 -0400

    Update user list view

--- src/web/helpers.js
+++ src/web/helpers.js
@@ -7,6 +7,24 @@ class Helpers {
     let formatted = dateFormat(datetime, 'dddd m/d/yy @ h:M TT')
     return formatted;
   }
+
+
+  static chunk(data, chunkSize){
+    let chunks = [];
+    // Start the chunk with -1 so the "group" clause increments on
+    // start to index 0
+    for (let index = 0, chunk = -1; index < data.length; index++){
+      if (index % chunkSize == 0){
+        chunk++;
+      }
+
+      // Grab the chunk and initialize the array if new, then append the data
+      chunks[chunk] = chunks[chunk] || [];
+      chunks[chunk].push(data[index])
+    }
+
+    return chunks;
+  }
 }
 
 module.exports = Helpers;

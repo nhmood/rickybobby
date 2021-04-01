2fbf53d8223333de67687ef899cf7d5ca377aa2b
Nima H. Mood <nhmood@goosecode.com>
2021-03-31 22:50:59 -0400

    Add helpers for running with cron + cron example

---
+++ cronjob.env.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# NVM needs the ability to modify your current shell session's env vars,
+# which is why it's a sourced function
+
+export NVM_DIR="/home/nhmood/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+
+# uncomment the line below if you need a specific version of node
+# other than the one specified as `default` alias in NVM (optional)
+# nvm use 4 1> /dev/null
---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+CUR=$(dirname $0)
+source $CUR/SECRETS
+export RB_DB=$CUR/db/rb.sqlite
+
+
+# $(which node) returns the path to the current node version
+# either the one specified as `default` alias in NVM or a specific version set above
+# executing `nvm use 4 1> /dev/null` here won't work!
+$(which node) ~/dev/rickybobby/run.js $1

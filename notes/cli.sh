ba70a32343bd4efb3cd564f4807f37b1517e42e0
Nima H. Mood <nhmood@goosecode.com>
2023-09-23 21:56:42 +0000

    Check in scratch files

---
+++ notes/cli.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+export RB_DB=./db/rb.sqlite
+export RB_USERNAME=nhmood
+export RB_USER=b5a692b93df84a2c97162f5564c9568b
+export RB_API_DATA=user_6abb5447981b41a1856e8bf653dfa332
+export RB_RIDE=750381e633584526ae95b2222b52cae1
+export RB_INSTRUCTOR=05735e106f0747d2a112d32678be8afd
+export RB_WORKOUT=0097838c52f44ed58083e8612017d5bc
+
+# ./run.js get api_data $RB_API_DATA
+
+# ./run.js get user $RB_USER
+# ./run.js get username $RB_USERNAME
+# ./run.js fetch username $RB_USERNAME
+
+# ./run.js get ride $RB_RIDE
+# ./run.js fetch ride $RB_RIDE
+
+# ./run.js get instructor $RB_INSTRUCTOR
+# ./run.js fetch instructor $RB_INSTRUCTOR
+
+# ./run.js get workout $RB_WORKOUT
+# ./run.js fetch workout $RB_WORKOUT
